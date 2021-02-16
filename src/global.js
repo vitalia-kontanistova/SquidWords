0beb8a98fa74c5ae68737bc40c974d6d5000ad5b
vitalia-kontanistova <v.konti@outlook.com>
2021-02-16 17:24:52 +0300

    Prepare details for work

---
+++ src/global.js
@@ -0,0 +1,16 @@
+export const colors = {
+  black: "#083948",
+  grey: "#D1CBB5",
+  white: "#FFFFFF",
+  /* ************** */
+  darkBlue: "#39A9BE",
+  blue: "#5CBFD1",
+  lightBlue: "#8DD7E5",
+  /* ************** */
+  green: "#478865",
+  lightGreen: "#94D1B0",
+  /* ************** */
+  darkRed: "#AF1D06",
+  red: "#F7573D",
+  lightRed: "#FFA589",
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import store from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
