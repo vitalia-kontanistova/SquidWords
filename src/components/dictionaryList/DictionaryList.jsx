85668398a3e6ce67e28ab08c62903358abcaa26d
vitalia-kontanistova <v.konti@outlook.com>
2021-02-28 20:27:07 +0300

    Refactoring dictionaryList and related components

--- src/components/dictionaryList/DictionaryList.jsx
+++ src/components/dictionaryList/DictionaryList.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import DictionaryTile from "../dictionaryTile/DictionaryTile";
-import { DictionaryListStyled } from "./DictionaryListStyled.styled";
+import {
+  DictionaryListStyled,
+  DictionaryTileStyled,
+  Cover,
+  Body,
+  Text,
+} from "./DictionaryListStyled.styled";
 
 const DictionaryList = (props) => {
   let dictionaryList = props.dictionaries.map((dict) => (
@@ -14,6 +19,21 @@ const DictionaryList = (props) => {
     />
   ));
   return <DictionaryListStyled>{dictionaryList}</DictionaryListStyled>;
+};
+
+const DictionaryTile = (props) => {
+  let { id, cover, title, descr, info } = props;
+
+  return (
+    <DictionaryTileStyled to={"/dictionary/" + id}>
+      <Cover src={cover} alt="" />
+      <Body>
+        <Text title="true">{title}</Text>
+        <Text descr="true">{descr}</Text>
+        <Text info="true">{info}</Text>
+      </Body>
+    </DictionaryTileStyled>
+  );
 };
 
 export default DictionaryList;
