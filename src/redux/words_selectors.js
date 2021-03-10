33ad7faa6983bd5f24fd65b9501b1a458478cbb9
vitalia-kontanistova <v.konti@outlook.com>
2021-03-10 17:20:54 +0300

    Added selector to chose randome word from array

--- src/redux/words_selectors.js
+++ src/redux/words_selectors.js
@@ -8,3 +8,25 @@ export const getWords = (state) => {
 export const getWordsToStudy = (state) => {
   return state.words.wordsToStudy;
 };
+
+export const getRandomWords = (words) => {
+  //cloning original array to avoid unforeseen changes there
+  let pickedWords = words.slice();
+
+  //forming array of "randome" indexes
+  let indexes = [];
+  for (let i = 0; i < 4; i++) {
+    indexes.push(Math.floor(Math.random() * pickedWords.length));
+  }
+
+  // moving "randome" pickedWords elements to the beginning
+  // to avoid repeated words
+  let tempWord;
+  for (let i = 0; i < indexes.length; i++) {
+    tempWord = pickedWords.splice(indexes[i], 1);
+    pickedWords = tempWord.concat(pickedWords);
+  }
+
+  //returning required amount (here it's 4) of "random" words
+  return pickedWords.splice(0, 4);
+};
