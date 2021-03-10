643e1cd70aac30489d327526f53c55a84c384a14
vitalia-kontanistova <v.konti@outlook.com>
2021-03-10 17:21:25 +0300

    Refactoring

--- src/redux/words_reducer.js
+++ src/redux/words_reducer.js
@@ -1,12 +1,46 @@
 import { wordAPI } from "../api/api";
-let initialState = {};
+
+const SET_SELECTED_WORD = "SET_SELECTED_WORD";
+const SET_BUNCH_OF_WORDS = "SET_BUNCH_OF_WORDS";
+const TOGGLE_SELECTED_WORD = "TOGGLE_SELECTED_WORD";
+
+let initialState = { selectedWords: [] };
 
 let wordsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_SELECTED_WORD:
+      return { ...state, selectedWords: [...state.selectedWords, action.word] };
+    case SET_BUNCH_OF_WORDS:
+      return { ...state, selectedWords: action.words };
+    case TOGGLE_SELECTED_WORD:
+      let hasCurrentWord = state.selectedWords.some(
+        (w) => w.wordId === action.word.wordId
+      );
+
+      if (hasCurrentWord) {
+        return {
+          ...state,
+          selectedWords: state.selectedWords.filter(
+            (w) => w.wordId !== action.word.wordId
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          selectedWords: [...state.selectedWords, action.word],
+        };
+      }
     default:
       return state;
   }
 };
+
+export let setSelectedWord = (word) => ({ type: SET_SELECTED_WORD, word });
+export let setSelectedWords = (words) => ({ type: SET_BUNCH_OF_WORDS, words });
+export let toggleSelectedWord = (word) => ({
+  type: TOGGLE_SELECTED_WORD,
+  word,
+});
 
 export let setKnowWordThunkCreator = (wordId) => (dispatch) => {
   wordAPI.setKnowWord(wordId).catch((error) => {
@@ -45,13 +79,17 @@ export let setDontKnowWordThunkCreator = (wordId) => (dispatch) => {
 };
 
 export let setKnowSelectedWordsThunkCreator = (wordsList) => (dispatch) => {
-  wordsList.forEach((word) => dispatch(setKnowWordThunkCreator(word.wordId)));
+  wordsList.forEach((word) => {
+    dispatch(setKnowWordThunkCreator(word.wordId));
+    dispatch(toggleSelectedWord(word));
+  });
 };
 
 export let setDontKnowSelectedWordsThunkCreator = (wordsList) => (dispatch) => {
-  wordsList.forEach((word) =>
-    dispatch(setDontKnowWordThunkCreator(word.wordId))
-  );
+  wordsList.forEach((word) => {
+    dispatch(setDontKnowWordThunkCreator(word.wordId));
+    dispatch(toggleSelectedWord(word));
+  });
 };
 
 export default wordsReducer;
